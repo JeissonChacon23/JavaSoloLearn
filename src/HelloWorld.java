16d03a5d8ae167471c470c8498ecdb5389b2ca3b
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-08 07:51:26 -0500

    Data types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
-public class HelloWorld {
+public class HelloWorld{
     public static void main(String[] args) throws Exception {
-        System.out.println("White once, run anywhere");
+        String name = "Jeisson";
+        int age = 27;
+        double height = 1.70;
+        float weight = 62.50f;
+        char sex = 'M';
+        boolean colombian = true;
+        System.out.println("\n\nName: "+ name +"\nAge: "+ age +"\nHeight: "+ height + "\nWeight: "+ weight +"\nSex: "+ sex +"\nColombian: "+ colombian);
     }
 }
