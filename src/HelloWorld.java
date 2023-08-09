8068a07ea7b828d52af162fa94f7936d277ce018
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-08 22:46:06 -0500

    Conditionals

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,34 @@
 import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) throws Exception {
-        //Input
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Name: ");
-        String name = sc.nextLine();
-        System.out.println("Age: ");
-        int age = sc.nextInt();
-        //nextDouble(), nextFloat(), nextBoolean().
-        System.out.println("Name: "+ name +"\nAge: "+ age);
+        //Conditionals
+        int age = 24;
+        if(age >= 18) {
+        System.out.println("Welcome");
+        }else {
+                System.out.println("Welcome!");
+            }
+        if(age > 0) {
+        if(age > 16) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Too Young");
+        }
+        } else {
+        System.out.println("Error");
+        }
 
-        System.out.println("Amount: ");
-        double amount = sc.nextDouble();
-        System.out.println(amount * 0.15);
+        if(age <= 0) {
+            System.out.println("Error");
+         }else if(age <= 16) {
+            System.out.println("Too Young");
+         } else if(age < 100) {
+            System.out.println("Welcome!");
+         } else {
+            System.out.println("Really?");
+         }
+
+         Scanner sc = new Scanner(System.in);
+        int temp = sc.nextInt();
     }
 }
