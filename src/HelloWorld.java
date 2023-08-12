ca3d4d868ebac25b0761e6e9297a1a5644550d5d
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-11 22:48:59 -0500

    Switch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,34 +1,46 @@
 import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) throws Exception {
-        //Conditionals
-        int age = 24;
-        if(age >= 18) {
-        System.out.println("Welcome");
-        }else {
-                System.out.println("Welcome!");
-            }
-        if(age > 0) {
-        if(age > 16) {
-            System.out.println("Welcome!");
-        } else {
-            System.out.println("Too Young");
-        }
-        } else {
-        System.out.println("Error");
+        //The Switch statement
+        int day = 2;
+
+        switch(day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
         }
 
-        if(age <= 0) {
-            System.out.println("Error");
-         }else if(age <= 16) {
-            System.out.println("Too Young");
-         } else if(age < 100) {
-            System.out.println("Welcome!");
-         } else {
-            System.out.println("Really?");
-         }
+        int x = 2;
+        switch(x) {
+        case 1: x = 2;
+        case 2: x = 3;
+        case 3: x = 4;
+        }
+        //System.out.println(null);
+        switch(day) {
+            case 1:
+              System.out.println("Monday");
+            case 2:
+              System.out.println("Tuesday");
+            case 3:
+              System.out.println("Wednesday");
+            default: 
+              System.out.println("Another day");
+          }
 
-         Scanner sc = new Scanner(System.in);
-        int temp = sc.nextInt();
+            int a = 11; int b = 12; int c = 40;
+            switch (a) {
+            case 40:
+            System.out.println(b);
+            break;
+            default:
+            System.out.println(c);
+}
     }
 }
