98daedc7c13f798e850574b12522737cd2e00193
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-12 09:38:01 -0500

    If else

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,46 +1,16 @@
 import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) throws Exception {
-        //The Switch statement
-        int day = 2;
-
-        switch(day) {
-        case 1:
-            System.out.println("Monday");
-            break;
-        case 2:
-            System.out.println("Tuesday");
-            break;
-        case 3:
-            System.out.println("Wednesday");
-            break;
+        //Multiple conditions
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        
+        if(age >= 0 && age <= 11){
+            System.out.println("Child");
+        }else if(age >= 12 && age <= 17){
+            System.out.println("Teen");
+        }else if(age >= 18 && age <= 64){
+            System.out.println("Adult");
         }
-
-        int x = 2;
-        switch(x) {
-        case 1: x = 2;
-        case 2: x = 3;
-        case 3: x = 4;
-        }
-        //System.out.println(null);
-        switch(day) {
-            case 1:
-              System.out.println("Monday");
-            case 2:
-              System.out.println("Tuesday");
-            case 3:
-              System.out.println("Wednesday");
-            default: 
-              System.out.println("Another day");
-          }
-
-            int a = 11; int b = 12; int c = 40;
-            switch (a) {
-            case 40:
-            System.out.println(b);
-            break;
-            default:
-            System.out.println(c);
-}
     }
 }
