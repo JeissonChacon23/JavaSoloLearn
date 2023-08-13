d1a2ca65967815c359f06b1f8bf48d25e62346cf
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-13 18:24:04 -0500

    For

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) throws Exception {
-        //While
-        int x = 3;
-        while(x > 0){
-            System.out.println(x + "\n");
-            x--;
+        //For
+        for (int i = 2; i < 10; i = i*i) {
+            System.out.println(i);
+          }
+
+        int x = 1;
+
+        while(x < 10) {
+              System.out.println(x);
+              if(x == 4) {
+                  break;
+              }
+              x++;
         }
 
-        //Do while
-        int y = 1;
-        do{
-            System.out.println(y);
-            y++;
-        }while(y < 5);
+        for(int a=10; a<=40; a+=10) {
+            if(a == 30) {
+                continue;
+            }
+            System.out.println(a);
+        }
     }
 }
