6946bd741395d28d0324a594f2a3b159fc9ab2bf
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-14 10:37:59 -0500

    Array

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) throws Exception {
-        //For
-        for (int i = 2; i < 10; i = i*i) {
-            System.out.println(i);
-          }
+        //Array
+        String[] names = new String[5];
+        names[0] = "Jeisson";
+        names[1] = "Javier";
+        System.out.println(names[0]);
+        String[ ] lastNames = { "A", "B", "C", "D"};
+        System.out.println(names[2]);
 
-        int x = 1;
-
-        while(x < 10) {
-              System.out.println(x);
-              if(x == 4) {
-                  break;
-              }
-              x++;
-        }
-
-        for(int a=10; a<=40; a+=10) {
-            if(a == 30) {
-                continue;
-            }
-            System.out.println(a);
+        String[] menu = {"Tea", "Espresso", "Americano", "Water", "Hot Chocolate"};
+        Scanner sc = new Scanner(System.in);
+        int option = sc.nextInt();
+        if(option < 5){
+            System.out.println(menu[option]);
+        }else{
+            System.out.println("Invalid");
         }
     }
 }
