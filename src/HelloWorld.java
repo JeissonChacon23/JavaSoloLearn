3fd601dbc9b38a2e03f3eb9ca3ba2f5f28cca13f
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-14 18:36:29 -0500

    Methods

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,34 +1,30 @@
 import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) throws Exception {
-        //Multidimensional arrays 
-        int[][] sample = {
-            {1, 2, 3},
-            {4, 5, 6}
-            };
-            for(int x=0; x<sample.length; x++) {
-                for(int y=0; y<sample[x].length; y++) {
-                    System.out.println(sample[x][y]);
-                }
-            } 
+        //Methods
+        Welcome();
+        Login();
+        bot();;
+    }
 
-        int[][] grid = {{8, 4}, {2, 5}, {9, 6}};
-        System.out.println(grid[2][1]);
-
-        int[][] seats = {
-            {0, 0, 0, 1, 1, 1, 0, 0, 1, 1},
-            {1, 1, 0, 1, 0, 1, 1, 0, 0, 0},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
-            {0, 1, 1, 1, 0, 0, 0, 1, 1, 1}
-            };
-            Scanner sc = new Scanner(System.in);
-            int row = sc.nextInt();
-            int column = sc.nextInt();
-            if(seats[row][column] == 0){
-                System.out.println("Free");
-            }else{
-                System.out.println("Sold");
-            }
+    
+    static void Welcome(){
+            System.out.println("Hello");
+        }
+    static void Login(){
+            System.out.println("Login");
+        }
+    static void bot(){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if(number == 1){
+            System.out.println("Order confirmed");
+        }else if(number == 2){
+            System.out.println("info@sololearn.com");
+        }else{
+            System.out.println("Try again");
+        }
     }
 }
+
+
