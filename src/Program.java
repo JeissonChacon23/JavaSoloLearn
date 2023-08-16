be8cdbd497bfad26a44e04d65b2941506742bb2b
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-16 17:55:56 -0500

    Final

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class Program {
+    public static final double PI = 3.1416; //Declaring a constant 
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
-        Counter c1 = new Counter();
-        Counter c2 = new Counter();
-        System.out.println(Counter.COUNT);
+        
     }
 }
