201a1185f3d299e3b06f5add2258dfb61d2d3deb
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-16 16:54:03 -0500

    Value & Reference Type

--- src/Program.java
+++ src/Program.java
@@ -4,16 +4,34 @@ public class Program {
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
 
-        Vehicle v1 = new Vehicle();
-        v1.setColor("Red");
-        System.out.println(v1.getColor());
+        int x = 5;
+        addOneTo(x);
 
-        //Test
-        Student s = new Student();
-        s.setAge(read.nextInt());
-        String name = "Jeisson";
-        s.name = name;
-        System.out.println(s.name);
-        System.out.println(s.getAge());
+        square(x);
+        System.out.println(x); 
+
+        Person j;
+        j = new Person("John");
+        j.setAge(20);
+        celebrateBirthday(j);
+        System.out.println(j.getAge());
+
+        Person p = new Person();
+        p.setAge(25);
+        change(p);
+        System.out.println(p.getAge());
+    }
+
+    static void celebrateBirthday(Person p) {
+        p.setAge(p.getAge() + 1);
+    }
+    static void addOneTo(int num){
+        num++;
+    }
+    static void square(int x) {
+        x = x*x;
+    }
+    static void change(Person p){
+        p.setAge(10);
     }
 }
