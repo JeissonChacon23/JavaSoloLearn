546fdf8cf46859e085f2fec66054ae43754e5010
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-16 07:53:34 -0500

    Class attributes

---
+++ src/Costumer.java
@@ -0,0 +1,13 @@
+public class Costumer {
+    String firstName;
+    String secondName;
+    int age;
+    int roomNumber;
+
+    public void saveCostumerInfo(){
+        System.out.println("First name: "+ firstName);
+        System.out.println("Second name: "+ secondName);
+        System.out.println("Age: "+ age);
+        System.out.println("Room number: "+ roomNumber);
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    int age;
+    double height;
+    String name;
+}
