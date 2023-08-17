4ce3fe08a04b5ef67c192e0e71da6acd698e0ab4
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-17 15:32:12 -0500

    Overloading

--- src/Program.java
+++ src/Program.java
@@ -3,9 +3,17 @@ import Polymorphism.*;
 public class Program {
     public static final double PI = 3.1416; //Declaring a constant 
     public static void main(String[] args){
-        Animal a = new Dog();
-        Animal b = new Cat();
-        a.makeSound();
-        b.makeSound();
+        int a = 5;
+        double b = 10.2;
+        System.out.println(doubleTheValue(a));
+        System.out.println(doubleTheValue(b));
+    }
+
+    public static int doubleTheValue(int a) {
+        return a * 2;
+    }
+    //overload the method for double value type
+    public static double doubleTheValue(double b) {
+        return b * 2;
     }
 }
