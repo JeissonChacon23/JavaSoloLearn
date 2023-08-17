455bbaed06fe9737fb5af52508c13c9a42bf76b9
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-17 10:01:25 -0500

    Inheritance

---
+++ src/Inheritance/Animal.java
@@ -0,0 +1,8 @@
+package Inheritance;
+public class Animal {
+    protected int legs;
+
+    public void eat(){
+        System.out.println("Animal eats");
+    }
+}
--- src/Program.java
+++ src/Program.java
@@ -1,17 +1,9 @@
-import java.util.Scanner;
-import Samples.*;
-import Encapsulation.*;
+import Inheritance.Dog;
 
 public class Program {
     public static final double PI = 3.1416; //Declaring a constant 
     public static void main(String[] args){
-        Scanner read = new Scanner(System.in);
-        Pupil pupil =  new Pupil();
-        pupil.setAge(read.nextInt());
-        if(pupil.getAge() > 6){
-            System.out.println("Welcome");
-        }else{
-            System.out.println("Sorry");
-        }
+        Dog d = new Dog();
+        d.eat();
     }
 }
