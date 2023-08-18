f3d32a5ddc83fc1cc8abc4a0b4ba787b77baecdd
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-18 10:00:29 -0500

    Enums

---
+++ src/Player.java
@@ -0,0 +1,15 @@
+public class Player {
+    Player(Difficulty diff){
+        switch(diff){
+            case EASY:
+                System.out.println("You have 3000 bullets");
+                break;
+            case MEDIUM:
+                System.out.println("You have 2000 bullets");
+                break;
+            case HARD:
+                System.out.println("You have 1000 bullets");
+                break;
+        }
+    }
+}
--- src/Program.java
+++ src/Program.java
@@ -2,9 +2,32 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
-        Animal a = new Animal("Robby");
-        Animal a1 = new Animal("Robby");
-        System.out.println(a == a1);
-        System.out.println(a.equals(a1));
+        enum Rank{
+            SOLDIER,
+            SERGEANT,
+            CAPTAIN
+        }
+
+
+
+        Rank a = Rank.SOLDIER;
+
+        switch(a){
+            case SOLDIER:
+                System.out.println("Soldier says, hi!");
+                break;
+            case SERGEANT:
+                System.out.println("Sergeant says, hi!");
+                break;
+            case CAPTAIN:
+                System.out.println("Captain says, hi!");
+                break;
+        }
     }
+}
+
+enum Difficulty{
+    EASY,
+    MEDIUM,
+    HARD
 }
