9304abab0182f49014bc4778ae39255f66876aa6
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-18 17:28:24 -0500

    Exception Handling

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,22 @@
 import java.awt.*;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
+        try{
+            int[] a = new int[2];
+            System.out.println(a[5]);
+        }catch(Exception e){
+            System.out.println("An error occured");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+	    int choice = scanner.nextInt();
+        String[] categories = {"PCs", "Notebooks", "Tablets", "Phones", "Аccessories"};
+        try{
+            System.out.println(categories[choice]);
+        }catch(Exception e){
+            System.out.println("Wrong Option");
+        }
     }
 }
