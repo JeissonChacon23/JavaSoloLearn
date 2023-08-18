fd3dc37a1d04a81bc6a3b1a3155cabd0ad30cba3
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-18 18:40:16 -0500

    Threads

---
+++ src/Loader.java
@@ -0,0 +1,5 @@
+public class Loader extends Thread{
+    public void run(){
+        System.out.println("Hello");
+    }
+}
--- src/Program.java
+++ src/Program.java
@@ -1,43 +1,7 @@
-import java.awt.*;
-import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 public class Program {
-    public void hacer(int x) throws IOException{
-                if(x < 0){
-                    throw new IOException();
-                }
-            }
-
-
-    static int div(int a, int b) throws ArithmeticException {
-        if(b == 0) {
-            throw new ArithmeticException("Division by Zero");
-        } else {
-            return a / b;
-        }
-    }
-
-
-
         public static void main(String[] args){
-        System.out.println(div(42, 0));
-
-        Scanner scanner = new Scanner(System.in);
-	    
-	    try {
-	        int num1 = scanner.nextInt();
-	        int num2 = scanner.nextInt();
-	        /*
-	        1. Error: division by zero
-	        2. Error: wrong value type
-	        */
-	        //your code goes here
-			System.out.println(num1/num2);
-	    } catch(ArithmeticException a1) {
-	        System.out.println("Error: division by zero");
-	    } catch(InputMismatchException e2){
-	        System.out.println("Error: wrong value type");
-	    }
+            Loader object = new Loader();
+            //object.run();
+            object.start();
     }
 }
