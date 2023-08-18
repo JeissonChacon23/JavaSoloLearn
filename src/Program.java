8c15318e25ad497ff0303bcffa427713a160a7f7
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-17 19:36:02 -0500

    Downcasting

--- src/Program.java
+++ src/Program.java
@@ -2,23 +2,8 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
-        Scanner read = new Scanner(System.in);
-        int a = (int) 3.14;    
-        System.out.println(a);
-        double b = 42.571;
-        int c = (int) b;
-        System.out.println(c);
-
-        double n = 1.5;
-        double m = 2.65;
-        sum((int)n, (int)m);
-
-        char x = read.next().charAt(0);
-        int y = (int) x;
-        System.out.println(y);
+        A object = new B();
+        B b = (B) object;
+        b.print();
     }
-
-    static void sum(int x, int y) {
-        System.out.println(x + y);
-     }
 }
