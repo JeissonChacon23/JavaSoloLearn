c9887c3ad486d11809df1f53f49fab9697c61605
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-18 09:41:00 -0500

    The equals() method

--- src/Person.java
+++
@@ -1,7 +0,0 @@
-public class Person {
-    class Hand{
-        public void shake(){
-            System.out.println("Hi!");
-        }
-    }
-}
--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
-        
+        Animal a = new Animal("Robby");
+        Animal a1 = new Animal("Robby");
+        System.out.println(a == a1);
+        System.out.println(a.equals(a1));
     }
 }
