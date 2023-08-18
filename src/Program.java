1b40571ed1acae4da417da4b7fb3e7629e7c208c
Jeisson Chacon <jeissonchaconjp@gmail.com>
2023-08-18 10:04:25 -0500

    Using the Java API

--- src/Program.java
+++ src/Program.java
@@ -1,33 +1,6 @@
-import java.util.Scanner;
+import java.awt.*;
 
 public class Program {
     public static void main(String[] args){
-        enum Rank{
-            SOLDIER,
-            SERGEANT,
-            CAPTAIN
-        }
-
-
-
-        Rank a = Rank.SOLDIER;
-
-        switch(a){
-            case SOLDIER:
-                System.out.println("Soldier says, hi!");
-                break;
-            case SERGEANT:
-                System.out.println("Sergeant says, hi!");
-                break;
-            case CAPTAIN:
-                System.out.println("Captain says, hi!");
-                break;
-        }
     }
-}
-
-enum Difficulty{
-    EASY,
-    MEDIUM,
-    HARD
 }
